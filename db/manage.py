803bebf57f52bf177b6edff794b8395956bab314
Sebastian Österlund <sirmcx@gmail.com>
2014-03-09 21:10:08 +0100

    DB upgrade testing

--- db/manage.py
+++ db/manage.py
@@ -2,8 +2,10 @@
 
 from migrate.versioning.shell import main
 import os
+from app import db
 
 if __name__ == '__main__':
+    db.create_all()
     db_url = os.environ.get('DATABASE_URL', 'postgresql://localhost:5432/cihui')
     db_url = db_url.replace('postgres:', 'postgresql:', 1)
     main(url=db_url, debug='False', repository='db')
