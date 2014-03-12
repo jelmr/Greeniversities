0e1d30376e54b2c9409a5dcb9a2ab16ee9bc50d9
Jelmer Mulder <jelmer_mulder@hotmail.com>
2014-03-12 12:25:58 +0100

    Heroku stuff.

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from flask.ext.lesscss import lesscss
 from app import app
-<<<<<<< HEAD
+
 lesscss(app)
-=======
->>>>>>> 6c76018483dcfe6ce7ddc6b8f9f7c816779d04c4
+
 app.run(host='0.0.0.0' ,debug = True)
