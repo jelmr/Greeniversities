712b54fa20ac43d6409cf1485ed29d4a873e55d3
Sebastian Österlund <sirmcx@gmail.com>
2014-03-12 21:07:53 +0100

    Heroku werkzeug test,

--- heroku.py
+++ heroku.py
@@ -1,5 +1,6 @@
 from app import app as frontend
 from backend import app as backend
+from werkzeug.serving import run_simple
 from werkzeug.wsgi import DispatcherMiddleware
 
 app = DispatcherMiddleware(frontend, {
